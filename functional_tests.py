c9c054e46152994e4f5a8562afc59f9450c5c344
garycheung13 <garyc102@gmail.com>
2018-05-07 12:12:48 -0400

    update functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,28 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+# functional tests subclass the TestCase class
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    # special function inherited from unittest parent class
+    # called before running tests
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    # special function inherited from unittest parent class
+    # called after running tests
+    def tearDown(self):
+        self.browser.quit()
+
+    # methods starting with test will be ran by test runner
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # check out homepage
+        self.browser.get('http://localhost:8000')
+        # user notices the page title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
