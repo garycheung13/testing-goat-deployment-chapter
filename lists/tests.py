ee4cd05eb966d339605987d77ae28b94df113a83
garycheung13 <garyc102@gmail.com>
2018-05-07 12:34:44 -0400

    create dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
+
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_homepage_view(self):
         found = resolve('/')
+        # asserting that the homepage matches the defined home_page function
         self.assertEqual(found.func, home_page)
+
+    def test_homepage_returns_correct_html(self):
+        request = HttpRequest() # dictionary containing the request's metadata
+        response = home_page(request) # pass into home_page view function to get a response object
+        html = response.content.decode('utf8') # decode response object
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
