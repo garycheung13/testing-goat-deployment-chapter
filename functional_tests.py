73c74f8f1ce40874f0163f5245e346b3658ead3f
garycheung13 <garyc102@gmail.com>
2018-05-07 19:04:49 -0400

    update unittests to use test client

--- functional_tests.py
+++ functional_tests.py
@@ -41,6 +41,7 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
+        # test passes if any row contains the item
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
--- lists/tests.py
+++ lists/tests.py
@@ -6,15 +6,22 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_homepage_view(self):
-        found = resolve('/')
-        # asserting that the homepage matches the defined home_page function
-        self.assertEqual(found.func, home_page)
+    # implicitly done when using test client
+    # def test_root_url_resolves_to_homepage_view(self):
+    #     found = resolve('/')
+    #     # asserting that the homepage function matches the home_page function defined in views
+    #     self.assertEqual(found.func, home_page)
 
     def test_homepage_returns_correct_html(self):
-        request = HttpRequest() # dictionary containing the request's metadata
-        response = home_page(request) # pass into home_page view function to get a response object
-        html = response.content.decode('utf8') # decode response object
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        # old test
+        # request = HttpRequest() # dictionary containing the request's metadata
+        # response = home_page(request) # pass into home_page view function to get a response object
+        # html = response.content.decode('utf8') # decode response object
+        # self.assertTrue(html.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', html)
+        # self.assertTrue(html.strip().endswith('</html>'))
+
+        # using django test client
+        response = self.client.get('/')
+        # checking for template match instead of trying to render to string
+        self.assertTemplateUsed(response, 'home.html')
