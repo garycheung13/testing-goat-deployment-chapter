50300d22601e7061ea2d5b898138602b31246c48
garycheung13 <garyc102@gmail.com>
2018-05-22 19:59:20 -0400

    add some basic validation with tests

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 # django functions
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 # client code
 from lists.models import Item, List
 
@@ -33,3 +34,16 @@ class ListandItemModelTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        # same as
+        # try:
+        #     item.save()
+        #     self.fail('The save should have raised an exception')
+        # except ValidationError:
+        #     pass
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean() # needed because Model doesn't to a full validation
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,7 +2,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
-
+from django.utils.html import escape
 # client code
 from lists.models import Item, List
 
@@ -46,6 +46,18 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first() # list was created when page posted to lists/new
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class ListViewTest(TestCase):
@@ -104,3 +116,4 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
