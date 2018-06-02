bb39332996407f825ff5cc3e3e57cc4cc4517983
garycheung13 <garyc102@gmail.com>
2018-06-01 23:36:40 -0400

    add jquery, qunit, keypress listener

--- lists/forms.py
+++ lists/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from lists.models import Item
 
@@ -12,6 +13,7 @@ from lists.models import Item
 #     )
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.models.ModelForm):
 
@@ -30,6 +32,24 @@ class ItemForm(forms.models.ModelForm):
 
     # overloading the save method from the parent modelform class
     def save(self, for_list):
-        # self.istance is an instance of the model object defined in class Meta
+        # self.instance is an instance of the model object defined in class Meta
         self.instance.list = for_list
         return super().save()
+
+
+class ExistingListItemForm(ItemForm):
+    def __init__(self, for_list, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.list = for_list
+
+    def validate_unique(self):
+        try:
+            self.instance.validate_unique()
+        except ValidationError as e:
+            # error_dict is a property of the validationError object
+            e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+            # update the error message
+            self._update_errors(e)
+
+    def save(self):
+        return forms.models.ModelForm.save(self)
